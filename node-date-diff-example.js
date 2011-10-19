50e34abf2dc18f28bf778907081a02e6461c0d8f
Shani Elharrar <atnt123@bezeqint.net>
2011-10-19 12:29:40 +0200

    Documentation for example

--- node-date-diff-example.js
+++ node-date-diff-example.js
@@ -1,29 +1,34 @@
 var formatFunction = function() {
 	
 	var numberToString = { 1 : "one", 2 : "two" };
-	var signToString = 	 { 1 : "After"};
+	var signToString = 	 { 1 : "In"};
 	signToString[-1] = "Before";
 
 	var pluralize = function(unit) {
 		return unit + "s";
 	}
 
+	// The format function.
 	return function(sign, number, unit) {
 		
+		// Pluralize the units if needed
 		if (number > 1) {
 			unit = pluralize(unit);
 		}
-	
+		
+		// This is nice, we can use natural language for some numbers...
 		if (number < 3) {
 			number = numberToString[number];
 		}
 
-	
-		console.log(signToString[sign] + " " + number + " " + unit);
+		// Return value
+		return signToString[sign] + " " + number + " " + unit;
 	}
 
 }();
 
-var dateDiff = require('./dates')(formatFunction);
+// Generating format and calculation function
+var dateDiff = require('./node-date-diff')(formatFunction);
 
-dateDiff(new Date(),new Date(2011, 9, 19));
+// Writing the diff to console.
+console.log(dateDiff(new Date(),new Date(2011, 9, 19)));
