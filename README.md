0769fafa6f50e1d32d516cde0c256674dac3f253
Shani Elharrar <atnt123@bezeqint.net>
2011-10-19 13:19:27 +0200

    Documentation

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Calculating date differences and formatting in a user-friendly way
 
+# Install
+
+    npm install node-date-diff
+
 # How ?
 
 See example in node-date-diff-example.js
--- node-date-diff-example.js
+++ node-date-diff-example.js
@@ -1,5 +1,5 @@
 // Generating format and calculation function
-var date_diff = require('lib/main');
+var date_diff = require('node-date-diff');
 
 var format_diff = date_diff.gen(date_diff.english);
 
