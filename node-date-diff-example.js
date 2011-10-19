ed3e8cd838be08ee54d40f4f38f52ce727bada26
Shani Elharrar <atnt123@bezeqint.net>
2011-10-19 13:26:08 +0200

    modified package.json, example

--- node-date-diff-example.js
+++ node-date-diff-example.js
@@ -1,7 +1,7 @@
 // Generating format and calculation function
 var date_diff = require('node-date-diff');
 
-var format_diff = date_diff.gen(date_diff.english);
+var format_diff = date_diff.gen(date_diff.formats.english);
 
 // Writing the diff to console.
 console.log(format_diff(new Date(),new Date(2011, 9, 18)));
